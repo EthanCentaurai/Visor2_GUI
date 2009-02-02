f66e870e05a0a5915509009d9175df150d769094
Ethan Centaurai <ethan.centaurai@live.co.uk>
2009-02-02 18:28:27 +0000

    Remove buggy, shitty, useless Virtual Frame support.

--- Core.lua
+++ Core.lua
@@ -35,14 +35,7 @@ function Visor2GUI:VISOR_UPDATE(p)
 	if not Visor2GUIFrame:IsShown() then return end
 
 	if p.f then
-		if self.virtual then
-			if p.f == "Visor2GUIVirtualFrame" or p.f == self.virtual then
-				self.f = self.virtual
-			end
-		else
-			self.f = p.f
-		end
-
+		self.f = p.f
 		self.parent = _G[self.f]:GetParent():GetName() or nil
 
 		if self.parent then Visor2GUIGrabParent:Enable()
@@ -79,11 +72,9 @@ function Visor2GUI:VISOR_UPDATE(p)
 		end
 
 		Visor2GUIHide:Enable()
-		Visor2GUIVirt:Enable()
 		Visor2GUIDelete:Enable()
 	else
 		Visor2GUIHide:Disable()
-		Visor2GUIVirt:Disable()
 		Visor2GUIDelete:Disable()
 	end
 end
@@ -105,78 +96,44 @@ end
 
 
 function Visor2GUI:EditBoxUpdate()
-	if self.virtual then
-		Visor2GUIEditBox:SetText(self.virtual)
-	else
-		Visor2:SetupFrame("f="..this:GetText())
-	end
+	Visor2:SetupFrame("f="..this:GetText())
 end
 
 function Visor2GUI:EditWUpdate()
 	local n = Visor2GUIEditW:GetText()
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." wh="..n)
-		Visor2:Do("f=Visor2GUIVirtualFrame wh="..n)
-	else
-		Visor2:Do("wh="..n)
-	end
+	Visor2:Do("wh="..n)
 end
 
 function Visor2GUI:ButtonWUp()
 	local n = Visor2GUIEditW:GetText() + 1
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." wh="..n)
-		Visor2:Do("f=Visor2GUIVirtualFrame wh="..n)
-	else
-		Visor2:Do("wh="..n)
-	end
+	Visor2:Do("wh="..n)
 end
 
 function Visor2GUI:ButtonWDown()
 	local n = Visor2GUIEditW:GetText() - 1
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." wh="..n)
-		Visor2:Do("f=Visor2GUIVirtualFrame wh="..n)
-	else
-		Visor2:Do("wh="..n)
-	end
+	Visor2:Do("wh="..n)
 end
 
 
 function Visor2GUI:EditHUpdate()
 	local n = Visor2GUIEditH:GetText()
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." ht="..n)
-		Visor2:Do("f=Visor2GUIVirtualFrame ht="..n)
-	else
-		Visor2:Do("ht="..n)
-	end
+	Visor2:Do("ht="..n)
 end
 
 function Visor2GUI:ButtonHUp()
 	local n = Visor2GUIEditH:GetText() + 1
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." ht="..n)
-		Visor2:Do("f=Visor2GUIVirtualFrame ht="..n)
-	else
-		Visor2:Do("ht="..n)
-	end
+	Visor2:Do("ht="..n)
 end
 
 function Visor2GUI:ButtonHDown()
 	local n = Visor2GUIEditH:GetText() - 1
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." ht="..n)
-		Visor2:Do("f=Visor2GUIVirtualFrame ht="..n)
-	else
-		Visor2:Do("ht="..n)
-	end
+	Visor2:Do("ht="..n)
 end
 
 
@@ -184,26 +141,14 @@ function Visor2GUI:ScaleUpdate()
 	local s = round(this:GetValue(), 2)
 
 	Visor2GUIScaleText:SetText(L["Frame Scale: "]..s)
-
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." s="..s)
-		Visor2:Do("f=Visor2GUIVirtualFrame s="..s)
-	else
-		Visor2:Do("s="..s)
-	end
+	Visor2:Do("s="..s)
 end
 
 function Visor2GUI:AlphaUpdate()
 	local a = round(this:GetValue(), 2)
 
 	Visor2GUIAlphaText:SetText(L["Frame Alpha: "]..a)
-
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." a="..a)
-		Visor2:Do("f=Visor2GUIVirtualFrame a="..a)
-	else
-		Visor2:Do("a="..a)
-	end
+	Visor2:Do("a="..a)
 end
 
 function Visor2GUI:HideUpdate()
@@ -220,26 +165,14 @@ end
 function Visor2GUI:EditXUpdate()
 	local x = Visor2GUIEditX:GetText()
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." x="..x)
-		Visor2:Do("f=Visor2GUIVirtualFrame x="..x)
-	else
-		Visor2:Do("x="..x)
-	end
-
+	Visor2:Do("x="..x)
 	Visor2GUIEditX:SetText(x)
 end
 
 function Visor2GUI:EditYUpdate()
 	local y = Visor2GUIEditY:GetText()
 
-	if self.virtual then
-		Visor2:Do("f="..self.virtual.." y="..y)
-		Visor2:Do("f=Visor2GUIVirtualFrame y="..y)
-	else
-		Visor2:Do("y="..y)
-	end
-
+	Visor2:Do("y="..y)
 	Visor2GUIEditY:SetText(y)
 end
 
@@ -249,110 +182,5 @@ function Visor2GUI:NudgeUpdate()
 
 	Visor2GUINudgeText:SetText(L["Nudge Amount: "]..n)
 	Visor2:Do("n="..n)
-end
-
-function Visor2GUI:NudgeLeft()
-	if self.virtual then
-		Visor2:Do("f="..self.virtual)
-		Visor2Nudge:Left()
-
-		Visor2:Do("f=Visor2GUIVirtualFrame")
-		Visor2Nudge:Left()
-	else
-		Visor2Nudge:Left()
-	end
-end
-
-function Visor2GUI:NudgeRight()
-	if self.virtual then
-		Visor2:Do("f="..self.virtual)
-		Visor2Nudge:Right()
-
-		Visor2:Do("f=Visor2GUIVirtualFrame")
-		Visor2Nudge:Right()
-	else
-		Visor2Nudge:Right()
-	end
-end
-
-function Visor2GUI:NudgeUp()
-	if self.virtual then
-		Visor2:Do("f="..self.virtual)
-		Visor2Nudge:Up()
-
-		Visor2:Do("f=Visor2GUIVirtualFrame")
-		Visor2Nudge:Up()
-	else
-		Visor2Nudge:Up()
-	end
-end
-
-function Visor2GUI:NudgeDown()
-	if self.virtual then
-		Visor2:Do("f="..self.virtual)
-		Visor2Nudge:Down()
-
-		Visor2:Do("f=Visor2GUIVirtualFrame")
-		Visor2Nudge:Down()
-	else
-		Visor2Nudge:Down()
-	end
-end
-
-
-function Visor2GUI:ButtonVirt()
-	local f = self.f
-	local vf = Visor2GUIVirtualFrame
-
-	if vf:IsShown() then
-		vf:Hide()
-		self.virtual = nil
-		return
-	end
-
-	if _G[f] then
-		self.virtual = _G[f]:GetName()
-
-		Visor2GUIVirtualFrameTitleString:SetText(f)
-		Visor2:Do("f=Visor2GUIVirtualFrame")
-
-		local h = _G[f]:GetHeight() or 200
-		local w = _G[f]:GetWidth() or 200
-		local s = _G[f]:GetScale() or 1
-		local x, y = _G[f]:GetCenter()
-		local p = _G[f]:GetParent()
-
-		if p then
-			p = _G[f]:GetParent():GetName()
-		else
-			p = "UIParent"
-		end
-
-		if not x then x = 500 end
-		if not y then y = 300 end
-
-		vf:SetParent(p)
-
-		if not vf.VisorSetScale then
-			vf:SetScale(s)
-		else
-			vf:VisorSetScale(s)
-		end
-
-		if not vf.VisorSetPoint then
-			vf:SetPoint("CENTER", p, "BOTTOMLEFT", x, y)
-		else
-			vf:VisorSetPoint("CENTER", p, "BOTTOMLEFT", x, y)
-		end
-
-		Visor2:Do("f=Visor2GUIVirtualFrame s="..s.." p1=CENTER p2=BOTTOMLEFT")
-		Visor2:Do("f=Visor2GUIVirtualFrame x="..x.." y="..y.." p="..p.." pr="..p)
-
-		if h and w then
-			Visor2:Do("f=Visor2GUIVirtualFrame".." ht="..h.." wh="..w)
-		end
-
-		vf:Show()
-	end
 end
 
